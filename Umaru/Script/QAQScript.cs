f0de5320e68907905a4970ddca532beca5af8d4f
liuqb <2378415@qq.com>
2024-09-25 15:37:24 +0800

    Update QAQScript.cs

--- Umaru/Script/QAQScript.cs
+++ Umaru/Script/QAQScript.cs
@@ -30,16 +30,16 @@ namespace Umaru.Script
 
 		public void ScriptMain()
 		{
-			
+
 			//参数1 模型 参数2 模型对应的字典
 			//var model = new YoloModel("xxx.onnx",new Dictionary<int, string> { });
 			//var yolo = new SuperYolo(model);
 
 			//yolo.Detect(87, 458, 143 - 87, 496 - 458);
 
-			//var ocr = new PaddleOCR();
-			//var result = ocr.Recognize(87, 458, 143 - 87, 496 - 458);
-			//var a = 1;
+			var ocr = new PaddleOCR();
+			var result = ocr.Recognize(87, 458, 143 - 87, 496 - 458);
+			Tools.Toast(result);
 		}
 	}
 }
